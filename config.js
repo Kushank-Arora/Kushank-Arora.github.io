fa2692dad070a5bc16d6d51adc92458d04998950
Kushank Arora <kushank97@gmail.com>
2026-06-26 03:05:22 +0200

    make it configurable

---
+++ config.js
@@ -0,0 +1,217 @@
+const myConfig = {
+  "warmup": [
+    {
+      "name": "Jumping Jacks",
+      "details": "60s. Increases heart rate.",
+      "videoId": "uLVt6u15L98"
+    },
+    {
+      "name": "Arm Circles & Swings",
+      "details": "30s each. Preps shoulders.",
+      "videoId": "3STTSi_jdHk"
+    },
+    {
+      "name": "Cat-Cow Stretch",
+      "details": "10 reps. Loosens spine.",
+      "videoId": "LIVJZZyZ2qM"
+    },
+    {
+      "name": "World's Greatest Stretch",
+      "details": "5 reps/side. Opens hips.",
+      "videoId": "7XheaZERvBQ"
+    },
+    {
+      "name": "Bodyweight Squats",
+      "details": "15 reps. Primes joints.",
+      "videoId": "m0GcZ24pK6k"
+    }
+  ],
+  "plan": {
+    "Monday": {
+      "location": "Gym 1",
+      "focus": "Upper Body Strength & Hypertrophy",
+      "exercises": [
+        {
+          "name": "Barbell Bench Press",
+          "sets": "4 sets of 6-8 reps",
+          "equipment": "Eleiko Rack & Bench",
+          "videoId": "hWbUlkb5Ms4"
+        },
+        {
+          "name": "TRX Inverted Rows",
+          "sets": "3 sets to failure",
+          "equipment": "TRX Suspension Trainers",
+          "videoId": "FyXAZW5zDhw"
+        },
+        {
+          "name": "Dumbbell Seated OHP",
+          "sets": "3 sets of 8-10 reps",
+          "equipment": "Dumbbells & Bench",
+          "videoId": "rO_iEImwHyo"
+        },
+        {
+          "name": "Keiser Cable Chest Flyes",
+          "sets": "3 sets of 12-15 reps",
+          "equipment": "Keiser Machine",
+          "videoId": "I-Ue34qLxc4"
+        },
+        {
+          "name": "Dumbbell Bicep Curls",
+          "sets": "3 sets of 10-12 reps",
+          "equipment": "Dumbbells",
+          "videoId": "cBSD6mQIPQk"
+        },
+        {
+          "name": "Tricep Pushdowns",
+          "sets": "3 sets of 12-15 reps",
+          "equipment": "Keiser Machine (Rope)",
+          "videoId": "NvZKjiZ8NYc"
+        }
+      ]
+    },
+    "Tuesday": {
+      "location": "Gym 2",
+      "focus": "Lower Body Strength & Core",
+      "exercises": [
+        {
+          "name": "Barbell Back Squats",
+          "sets": "4 sets of 6-8 reps",
+          "equipment": "Barbell & Squat Rack",
+          "videoId": "gcNh17Ckjgg"
+        },
+        {
+          "name": "Kettlebell Goblet Squats",
+          "sets": "3 sets of 10-12 reps",
+          "equipment": "Kettlebells",
+          "videoId": "QilYZ20kVfM"
+        },
+        {
+          "name": "Leg Extensions",
+          "sets": "3 sets of 12-15 reps",
+          "equipment": "BLK BOX Leg Extension",
+          "videoId": "uM86QE59Tgc"
+        },
+        {
+          "name": "Leg Curls",
+          "sets": "3 sets of 12-15 reps",
+          "equipment": "BLK BOX Leg Curl",
+          "videoId": "t9sTSr-JYSs"
+        },
+        {
+          "name": "Keiser Cable Crunches",
+          "sets": "3 sets of 15 reps",
+          "equipment": "Keiser Machine (Rope)",
+          "videoId": "CDO29I7PCoc"
+        }
+      ]
+    },
+    "Wednesday": {
+      "location": "Gym 1",
+      "focus": "Full Body HIIT Conditioning",
+      "exercises": [
+        {
+          "name": "Concept2 SkiErg",
+          "sets": "5 rounds x 250m",
+          "equipment": "Concept2 SkiErg",
+          "videoId": "B0lIgT5PHc8"
+        },
+        {
+          "name": "Kettlebell Swings",
+          "sets": "4 sets of 15 reps",
+          "equipment": "Kettlebells",
+          "videoId": "RU88iqRVunk"
+        },
+        {
+          "name": "Keiser Woodchoppers",
+          "sets": "3 sets of 12 reps/side",
+          "equipment": "Keiser Machine (Handle)",
+          "videoId": "pAplQXk3dkU"
+        },
+        {
+          "name": "AssaultBike Finisher",
+          "sets": "10 mins (15s sprint/45s slow)",
+          "equipment": "AssaultBike",
+          "videoId": "Fv8tIEJhcsc"
+        }
+      ]
+    },
+    "Thursday": {
+      "location": "Gym 2",
+      "focus": "Upper Body Pump Day",
+      "exercises": [
+        {
+          "name": "Barbell Overhead Press",
+          "sets": "4 sets of 8-10 reps",
+          "equipment": "Barbell & Rack",
+          "videoId": "4LBVP2Oe7fg"
+        },
+        {
+          "name": "DB Incline Bench Press",
+          "sets": "3 sets of 8-12 reps",
+          "equipment": "Dumbbells & Incline Bench",
+          "videoId": "8iPEnn-ltC8"
+        },
+        {
+          "name": "DB Single-Arm Rows",
+          "sets": "3 sets of 10-12 reps/arm",
+          "equipment": "Dumbbells & Flat Bench",
+          "videoId": "qN54-QNO1eQ"
+        },
+        {
+          "name": "Keiser Lateral Raises",
+          "sets": "3 sets of 15 reps/arm",
+          "equipment": "Keiser Machine",
+          "videoId": "f_OGBg2KxgY"
+        },
+        {
+          "name": "Keiser Cable Bicep Curls",
+          "sets": "3 sets of 12 reps",
+          "equipment": "Keiser Machine",
+          "videoId": "2MUEL4nL6hA"
+        },
+        {
+          "name": "Keiser Tricep Ext.",
+          "sets": "3 sets of 12 reps",
+          "equipment": "Keiser Machine (Rope)",
+          "videoId": "b5le--KkyH0"
+        }
+      ]
+    },
+    "Saturday": {
+      "location": "Gym 2",
+      "focus": "Lower Body Hypertrophy & Carry",
+      "exercises": [
+        {
+          "name": "Barbell RDLs",
+          "sets": "4 sets of 8-10 reps",
+          "equipment": "Barbell & Weights",
+          "videoId": "5rIqP63yWFg"
+        },
+        {
+          "name": "Dumbbell Walking Lunges",
+          "sets": "3 sets of 12 steps/leg",
+          "equipment": "Dumbbells",
+          "videoId": "mJilHWIBWO8"
+        },
+        {
+          "name": "Leg Extensions",
+          "sets": "3 sets of 15 reps",
+          "equipment": "BLK BOX Leg Extension",
+          "videoId": "uM86QE59Tgc"
+        },
+        {
+          "name": "Leg Curls",
+          "sets": "3 sets of 15 reps",
+          "equipment": "BLK BOX Leg Curl",
+          "videoId": "t9sTSr-JYSs"
+        },
+        {
+          "name": "DB Carries & KB Swings",
+          "sets": "4 rounds (40 paces + 15 swings)",
+          "equipment": "Dumbbells & Kettlebell",
+          "videoId": "1uOs1hP3u4A"
+        }
+      ]
+    }
+  }
+}
