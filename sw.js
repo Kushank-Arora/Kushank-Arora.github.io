44cdc75fd9bfeed19593d5a0ab65751d0c6286d3
Kushank Arora <kushank97@gmail.com>
2026-06-26 02:24:50 +0200

    make it a pwa

---
+++ sw.js
@@ -0,0 +1,25 @@
+const cacheName = 'exercise-app-v1';
+const filesToCache = [
+  './',
+  './index.html',
+  './logo.png',
+  './manifest.json'
+];
+
+// Install Service Worker and cache essential files
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+// Network-first strategy with cache fallback
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
+  );
+});
